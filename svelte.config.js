fc9bede37366ef5e2f0753ef6599f4c8e180f7d6
TorinSamples <torin.samples@gmail.com>
2025-01-30 13:24:24 -0800

    deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+    }
+  }
 };
 
 export default config;
